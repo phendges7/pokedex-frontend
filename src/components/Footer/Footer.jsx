eb1491080792ec118ae250dd5489cdc52a58aff0
phendges7 <uk_hendges@outlook.com>
2025-05-09 17:16:54 +0100

    moving contact icons to header

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,30 +7,6 @@ const Footer = () => {
       <p className="footer__copyright">
         &copy; 2025 POKEDEX - TUDO NOSSO, NADA DELES
       </p>
-      <ul className="footer__social">
-        <li className="footer__social-item">
-          <a
-            href="https://www.linkedin.com/in/phendges7/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src={instagram}
-              alt="Instagram"
-              className="footer__social-icon"
-            />
-          </a>
-        </li>
-        <li className="footer__social-item">
-          <a
-            href="https://github.com/phendges7"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={github} alt="GitHub" className="footer__social-icon" />
-          </a>
-        </li>
-      </ul>
     </footer>
   );
 };
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,7 @@
 import headerLogo from "../../assets/headerLogo.png";
 import home from "../../assets/home-icon.svg";
+import instagram from "../../assets/instagram-icon.svg";
+import github from "../../assets/github-icon.svg";
 import SearchForm from "../SearchForm/SearchForm";
 import { useNavigate } from "react-router-dom";
 
@@ -23,6 +25,30 @@ const Header = () => {
 
       <div className="header__right">
         <SearchForm />
+        <ul className="header__social">
+          <li className="header__social-item">
+            <a
+              href="https://www.linkedin.com/in/phendges7/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={instagram}
+                alt="Instagram"
+                className="header__social-icon"
+              />
+            </a>
+          </li>
+          <li className="header__social-item">
+            <a
+              href="https://github.com/phendges7"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={github} alt="GitHub" className="header__social-icon" />
+            </a>
+          </li>
+        </ul>
       </div>
     </header>
   );
